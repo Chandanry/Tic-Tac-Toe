52773c7fa8c2c677e05461f856afca04597e0523
Chandan <161803238+Chandanry@users.noreply.github.com>
2024-08-17 11:41:42 +0530

    script.js

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let resetBtn = document.querySelector("#reset");
 let newGame = document.querySelector("#gamebtn");
 let msg = document.querySelector(".msg");
 let msp = document.querySelector("#ms");
+let a = document.querySelector("#a");
 
 let turnO = true;   //player1 or player2
+let moveCount = 0;  // Track the number of moves made
 
-//winning patterns in 2D-arrays
-
+// Winning patterns in 2D-arrays
 const winpatterns = [
     [0, 1, 2],
     [0, 3, 6],
@@ -20,67 +21,77 @@ const winpatterns = [
 ];
 
 boxes.forEach((box) => {
-    box.addEventListener("click",()=>{         
-       if(turnO){
-        box.innerText = "O";
-        turnO = false;
-       }else{
-        box.innerText = "X";
-        turnO = true;
-       }
-       box.disabled = true;
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        moveCount++;                    // Increment move count with each click
 
-       checkWinner();
-
+        checkWinner();
     });
 });
 
-const disable = () =>{
-    for(let box of boxes){
+const disable = () => {
+    for (let box of boxes) {
         box.disabled = true;
     }
 }
-const enable = () =>{
-    for(let box of boxes){
+
+const enable = () => {
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
+    moveCount = 0;  // Reset move count. this will count the button clicks.
 }
+
 const showWinner = (winner) => {
-    msp.innerText = `Congratulations, winner is ${winner}`;
+    msp.innerText = `Congratulations, the winner is ${winner}!`;
+    a.innerText = "Start a New Game...";
     msg.classList.remove("hide");
-
     disable();
-
+}
+// this is the additional of tic tac toe i.e is DRAW
+const showDraw = () => {
+    msp.innerText = "It's a draw!";
+    a.innerText = "Start a New Game...";
+    msg.classList.remove("hide");
+    disable();
 }
 
 const checkWinner = () => {
-    for(let pattern of winpatterns){
-        // console.log(pattern[0],pattern[1],pattern[2]);
-        // console.log(
-        //     boxes[pattern[0]].innerText,    // position 1
-        //     boxes[pattern[1]].innerText,    // position 2
-        //     boxes[pattern[2]].innerText
-        // );                                     // position 3
-
+    let winnerFound = false;          // at start the winner is false....
+    
+    for (let pattern of winpatterns) {
         let post1Val = boxes[pattern[0]].innerText;
         let post2Val = boxes[pattern[1]].innerText;
         let post3Val = boxes[pattern[2]].innerText;
 
-         if(post1Val !== ""  && post2Val !== "" && post3Val !=="") {
-            if(post1Val == post2Val && post3Val){
+        if (post1Val !== "" && post2Val !== "" && post3Val !== "") {
+            if (post1Val === post2Val && post2Val === post3Val) {
                 showWinner(post1Val);
+                winnerFound = true;
+                break;
             }
-         }
-
+        }
+    }
+    
+    //           If no winner found and all moves are made, declare a draw
+    if (!winnerFound && moveCount === 9) {    // button clicks = 9 then its a draw and showDraw function will be called.
+        showDraw();
     }
 }
 
 const resetGame = () => {
-turnO = true;
-enable();
-msg.classList.add("hide");
+    turnO = true;
+    enable();
+    msg.classList.add("hide");
 }
 
-newGame.addEventListener("click",resetGame);
-resetBtn.addEventListener("click",resetGame);
+newGame.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
